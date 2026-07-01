b592f83a74aad44d95216ec8335228fa483b77c3
KagurazakaNyaa <dev@kagurazakanyaa.com>
2026-07-01 08:42:04 +0800

    feat: Bevy Plugin scaffolding

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+use bevy::prelude::*;
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct DepotStorageModPlugin;
+
+impl Plugin for DepotStorageModPlugin {
+    fn build(&self, _app: &mut App) {}
+}
